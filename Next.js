e9e6357449e85f4c5df8a8236d66be14f390c815
Berke Sertel <76247074+SweetRomeo@users.noreply.github.com>
2025-01-08 15:55:36 +0300

    Update Next.js

--- Next.js
+++ Next.js
@@ -1,1 +1,7 @@
 Next js nedir?
+Next js bir framework yani kütüphaneler topluluğudur. Next.js reactın üzerine inşa edilen bir şeydir.
+Next js içerisindeki işlevler react ile uyumlu olacak şekilde tasarlanmıştır.  
+Rendering Nedir?
+Sayfaların html'e dönüştürülmesi işlemine verilen isimdir.
+  
+  
